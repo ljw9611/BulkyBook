8d829205af23f8d458baeaad261c714bc8fd76d2
Garden <66628342+ljw9611@users.noreply.github.com>
2023-03-09 23:58:28 +0900

    Create Category Model

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 ﻿using BulkyBookWeb.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc; //MVC에서 컨트롤러를 작성하기 위해 사용
+using System.Diagnostics; //디버그와 관련된 기능을 제공
 
 namespace BulkyBookWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller //HomeController 클래스를 정의,  Controller 클래스를 상속받음
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger; // 로깅을 위해 사용됩니다.
 
         public HomeController(ILogger<HomeController> logger)
         {
