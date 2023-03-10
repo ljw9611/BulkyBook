beff698583348555b858fdfd35be038664109bf3
Garden <66628342+ljw9611@users.noreply.github.com>
2023-03-10 15:38:03 +0900

    Add Connection String / Add ApplicationDbContext / Setup Program.cs to use DbContext

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore; //패키지 설치
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
